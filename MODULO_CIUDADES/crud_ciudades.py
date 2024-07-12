623b0f2b5ca8a92d831825ed6033347130613a21
VictorMarimon <3reehuy@gmail.com>
2024-07-12 14:58:32 -0500

    feat: Se añade el archivo crud_ciudades.py que permite la gestión de los datos

---
+++ MODULO_CIUDADES/crud_ciudades.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+sys.path.append("..")
+
+from datosGenerales.datos import *
+from modulo_reportes.reportes_excepciones import agregar_reportes
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+RUTA_JSON = os.path.join(project_root,  "MODULO_DATOS", "ciudades.json")
+
+def agregar_ciudad():
+    ciudades = cargar_datos(RUTA_JSON)
+
+    for i in ciudades:
+        print(i)
+
+agregar_ciudad()
